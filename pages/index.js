970581908837485ef4327fadc88694c070ea881f
Vladyslav Bezruk <vladyslavbezruk@yahoo.com>
2024-04-04 19:11:28 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Vladyslav Bezruk page!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Coming soon ...
         </p>
       </main>
 
